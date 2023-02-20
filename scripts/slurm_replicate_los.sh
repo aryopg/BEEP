907d7c7f5f373b0115d7b29116ed25bb9fe615bc
Aryo Pradipta Gema <aryopg@gmail.com>
2023-02-20 09:24:15 +0000

    successfully replicate LOS bluebert with softvoting

--- scripts/slurm_replicate_los.sh
+++ scripts/slurm_replicate_los.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=14000  # memory in Mb
 #SBATCH --partition=ILCC_GPU
 #SBATCH -t 6-00:00:00  # time requested in hour:minute:seconds
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=3
 
 echo "Job running on ${SLURM_JOB_NODELIST}"
 
@@ -33,27 +33,35 @@ dest_dataset_path=${SCRATCH_HOME}/${PROJECT_NAME}/datasets/
 mkdir -p ${dest_dataset_path}  # make it if required
 rsync --archive --update --compress --progress ${src_path}/ ${dest_dataset_path}
 
-src_model_checkpoint_path=/home/${USER}/${PROJECT_NAME}/data/models/outcome-models/los_bluebert_softvote_k5.pt
-dest_model_checkpoint_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/models/outcome-models/los_bluebert_softvote_k5.pt
+src_model_checkpoint_path=/home/${USER}/${PROJECT_NAME}/data/models/outcome-models/
+dest_model_checkpoint_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/models
 mkdir -p ${dest_model_checkpoint_path}  # make it if required
+dest_model_checkpoint_path=${dest_model_checkpoint_path}/outcome-models
+mkdir -p ${dest_model_checkpoint_path}
 rsync --archive --update --compress --progress ${src_model_checkpoint_path} ${dest_model_checkpoint_path}
+
+src_retrieved_docs_path=/home/${USER}/${PROJECT_NAME}/data/los_reranked_final/
+dest_retrieved_docs_path=${SCRATCH_HOME}/${PROJECT_NAME}/data/los_reranked_final/
+mkdir -p ${dest_retrieved_docs_path}  # make it if required
+rsync --archive --update --compress --progress ${src_retrieved_docs_path} ${dest_retrieved_docs_path}
 
 OUTPUT_DIR=${SCRATCH_HOME}/${PROJECT_NAME}/outputs/
 
 echo "Running LOS benchmarking"
 cd outcome-prediction/
-python run_outcome_prediction.py 
-  --train ${dest_path}/length_of_stay/LOS_WEEKS_adm_train.csv \
-  --dev ${dest_path}/length_of_stay/LOS_WEEKS_adm_val.csv \
-  --test ${dest_path}/length_of_stay/LOS_WEEKS_adm_test.csv \
-  --init_model "bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12" \
-  --out_dir ${OUTPUT_DIR} \
-  --checkpoint ${dest_model_checkpoint_path} \
-  --outcome los \
-  --do_test \
-  --strategy average \
-  --lit_dir ../data/los_reranked_final \
-  --num_top_docs 5
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CONDA_PREFIX}/lib
+python run_outcome_prediction.py \
+--train ${dest_dataset_path}/length_of_stay/LOS_WEEKS_adm_train.csv \
+--dev ${dest_dataset_path}/length_of_stay/LOS_WEEKS_adm_val.csv \
+--test ${dest_dataset_path}/length_of_stay/LOS_WEEKS_adm_test.csv \
+--init_model "bionlp/bluebert_pubmed_mimic_uncased_L-12_H-768_A-12" \
+--out_dir ${OUTPUT_DIR} \
+--checkpoint ${dest_model_checkpoint_path}/los_bluebert_softvote_k5.pt \
+--outcome los \
+--do_test \
+--strategy average \
+--lit_dir ${dest_retrieved_docs_path} \
+--num_top_docs 5
 
 
 OUTPUT_HOME=${PWD}/exps/
